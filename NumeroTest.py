24292991cc227aae81df126bdd47f7e4a8d8d5bc
Cristian Sepulveda <cf.sepulveda@uniandes.edu.co>
2019-04-02 22:46:57 -0500

    Paso 4 String vacio (Rojo)

--- NumeroTest.py
+++ NumeroTest.py
@@ -44,3 +44,8 @@ class NumeroTest(TestCase):
 
     def test_procesar_cadena_cinco_numeros_cadena_tres(self):
         self.assertEqual(Numero().resolverTres("1, 4, 4 ,3, 4"), [5, 1, 4], "5 numero")
+
+
+        #Iteración 4
+    def test_procesarCadena_cuatro(self):
+        self.assertEqual(Numero().resolverCuatro(""), [0, 0, 0, 0], "Cadena vacía")
