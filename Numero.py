5b7185aac215f449881967ae61a08819aab8aad1
Cristian Sepulveda <cf.sepulveda@uniandes.edu.co>
2019-04-02 22:47:10 -0500

    Paso 4 String vacio (Verde)

--- Numero.py
+++ Numero.py
@@ -24,3 +24,11 @@ class Numero:
             return [0, 0, 0] 
         else:
             return [len(arrNumber), float(min(arrNumber)),  float(max(arrNumber))]     
+
+    def resolverCuatro(self, cadena):
+        cadena = cadena.replace(' ', '')
+        arrNumber = cadena.split(",")
+        if not cadena:
+            return [0, 0, 0, 0] 
+        else:
+            return [len(arrNumber), float(min(arrNumber)),  float(max(arrNumber))]     
