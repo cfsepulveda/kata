9975103cacfc60d6f51f26a9d6fc64f6d6bba7e1
Cristian Sepulveda <cf.sepulveda@uniandes.edu.co>
2019-04-02 22:29:44 -0500

    Paso 3 String Vacio (Verde)

--- Numero.py
+++ Numero.py
@@ -16,3 +16,11 @@ class Numero:
             return [0, 0] 
         else:
             return [len(arrNumber), float(min(arrNumber))]     
+
+    def resolverTres(self, cadena):
+        cadena = cadena.replace(' ', '')
+        arrNumber = cadena.split(",")
+        if not cadena:
+            return [0, 0, 0] 
+        else:
+            return [len(arrNumber), float(min(arrNumber)), 0]     
